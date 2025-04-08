c942ea16300f4f8d11518514e515bc5b3df74623
HarryHoan <thongsieusao3@gmail.com>
2025-04-08 23:20:36 +0700

    modify the getPledgeFromProjectId to getBackerFromProjectId

---
+++ Application/ViewModels/PledgeDTO/ProjectBackerDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.ViewModels.PledgeDTO
+{
+    public class ProjectBackerDto
+    {
+        public int? backerId { get; set; }
+        public string backerName { get; set; } = string.Empty;
+        public string backerAvatar { get; set; } = string.Empty;
+        public PledgeDto pledge { get; set; } = null!;
+    }
+}
