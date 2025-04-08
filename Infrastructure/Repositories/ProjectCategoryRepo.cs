72174348f4384603f01d2ecdc9076848a2fad37f
HarryHoan <thongsieusao3@gmail.com>
2025-04-08 15:42:20 +0700

    Adding GetAllPlatformByProjectId and GetAllProjectByPlatformId in platform, Adding GetAllProjectByCategoryId, Updating excel file

--- Infrastructure/Repositories/ProjectCategoryRepo.cs
+++ Infrastructure/Repositories/ProjectCategoryRepo.cs
@@ -18,6 +18,16 @@ namespace Infrastructure.Repositories
                 .Include(pc => pc.Category)
                 .ToListAsync();
         }
+        public async Task<List<ProjectCategory>> GetAllProjectByCategoryAsync(int categoryId)
+        {
+            var projectCategories = await _context.ProjectCategories
+                .Where(pc => pc.CategoryId == categoryId)
+                .Include(pc => pc.Project)
+                .OrderBy(pc => pc.Project.Title) // Sort by Project Name
+                .ToListAsync();
 
+            // Return an empty list if no records are found
+            return projectCategories ?? new List<ProjectCategory>();
+        }
     }
 }
